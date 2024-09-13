31c54a1526e0557b30d9ddbe6cad668feb8d1379
J-USF-B <brown827@usf.edu>
2024-09-12 20:44:52 -0400

    Add files via upload

---
+++ Module 3.R
@@ -0,0 +1,5 @@
+name <- c("Jeb", "Donald", "Ted", "Marco", "Carly", "Hillary", "Bernie")
+abc_polls <- c(4, 62, 51, 21, 2, 14, 15)
+cbs_polls <- c(12, 75, 43, 19, 1, 21, 19)
+
+df <- data.frame(Names = name, ABC = abc_polls, CBS = cbs_polls)
